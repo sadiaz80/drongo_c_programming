8a20b5d834fb1b1b74dce35b53f1d70f3edbe939
sadiaz80 <sadikabdirizak@gmail.com>
2023-01-25 14:59:03 +0300

    day 1

--- week 3/ex.c
+++ week 3/ex.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
-int sq(int n);
+int sq(int n) ;
 
 int main()
 {
 
-    int number = 5, results;
+    int number , results;     
 
-    results = sq(number);
+    results = sq(number) * sq(number) ;
 
-    printf("the square of 5 = %d\n", results);
+    
+
+    //printf("the square number");
+
+//    scanf("%i\n", &number);
+
+    printf("the square of 5 = %ls\n", &results);
 }
 
-int sq(int n)
-{
+
+int sq(int n)  {
+
+return n * n ;
+
+
+
+
+
+
+
+
+}
+
+//int sq(int n)
 
     // if (n = 5)
     //     return n * n;
     // else
     //     return n;
 
-    return n * n;
-}
+  //  return n * n;
+
